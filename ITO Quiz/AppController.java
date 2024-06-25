7ab240fb2567169af7752d88a5ae3c64993077c0
Nanda-Kumar-L <165262946+Nanda-Kumar-L@users.noreply.github.com>
2024-06-25 17:53:37 +0530

    Latest Update

--- ITO Quiz/AppController.java
+++ ITO Quiz/AppController.java
@@ -5,7 +5,7 @@ import com.marklogicquery.run.xquery.model.Candidate;
 import com.marklogicquery.run.xquery.model.Question;
 import com.marklogicquery.run.xquery.repository.AdminRepository;
 import com.marklogicquery.run.xquery.repository.CandidateRepository;
-import com.marklogicquery.run.xquery.repository.PathRangeIndex;
+import com.marklogicquery.run.xquery.repository.PathRangeIndexRepository;
 import com.marklogicquery.run.xquery.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,22 +17,22 @@ import java.util.List;
 public class AppController {
 
     @Autowired
-    private SequenceGenerator sg1;
+    private QuestionSetIdGenerator questionSetIdGenerator;
 
     @Autowired
-    private SequenceGenerator2 sg2;
+    private QuestionIdGenerator questionIdGenerator;
 
     @Autowired
-    private SequenceGenerator3 sg3;
+    private CandidateIdGenerator candidateIdGenerator;
 
     @Autowired
-    private SequenceGenerator4 sg4;
+    private AnswerIdGenerator answerIdGenerator;
 
     @Autowired
     private AdminRepository adminRepository;
 
     @Autowired
-    private PathRangeIndex pathRangeIndex;
+    private PathRangeIndexRepository pathRangeIndexRepository;
 
     @Autowired
     private CandidateRepository candidateRepository;
@@ -40,19 +40,19 @@ public class AppController {
     @PostMapping("/createQuestions")
     public String create_questions(@RequestBody List<Question> question)
     {
-        pathRangeIndex.createPathRangeIndexes();
-        sg1.reset(Long.parseLong(sg1.getSequence().trim()));
-        long id1 = sg1.nextId();
+        pathRangeIndexRepository.createPathRangeIndexes();
+        questionSetIdGenerator.setId(Long.parseLong(questionSetIdGenerator.fetchIdFromDatabase().trim()));
+        long id1 = questionSetIdGenerator.nextId();
         adminRepository.CreateQuestionSet(id1);
         StringBuilder sb = new StringBuilder();
         for(Question q:question) {
-            sg2.reset(Long.parseLong(sg2.getSequence().trim()));
-            long id2 = sg2.nextId();
+            questionIdGenerator.setId(Long.parseLong(questionIdGenerator.fetchIdFromDatabase().trim()));
+            long id2 = questionIdGenerator.nextId();
             q.setQuestion_id(String.valueOf(id2));
             sb.append(adminRepository.createQuestion(q, id1));
-            sg2.setSequence(id2);
+            questionIdGenerator.updateIdInDatabase(id2);
         }
-        sg1.setSequence(id1);
+        questionSetIdGenerator.updateIdInDatabase(id1);
         return sb.toString();
     }
 
@@ -62,20 +62,20 @@ public class AppController {
         return adminRepository.getAllQuestions();
     }
 
-    @GetMapping("/getQuestionSetById/{id}")
-    public String get_question_set_by_id(@PathVariable String id)
+    @GetMapping("/getQuestionSetById")
+    public String get_question_set_by_id(@RequestParam String questionSetId)
     {
-        return adminRepository.getQuestionSetById(id);
+        return adminRepository.getQuestionSetById(questionSetId);
     }
 
-    @GetMapping("/getQuestionById/{questionSetId}/{questionId}")
-    public String get_question_by_id(@PathVariable String questionSetId,@PathVariable String questionId)
+    @GetMapping("/getQuestionById")
+    public String get_question_by_id(@RequestParam String questionSetId,@RequestParam String questionId)
     {
         return adminRepository.getQuestionById(questionSetId,questionId);
     }
 
-    @PutMapping("/updateQuestion/{questionSetId}/{questionId}")
-    public String update_question_by_id(@PathVariable String questionSetId,@PathVariable String questionId,@RequestBody List<Question> question)
+    @PutMapping("/updateQuestion")
+    public String update_question_by_id(@RequestParam String questionSetId,@RequestParam String questionId,@RequestBody List<Question> question)
     {
         StringBuilder sb = new StringBuilder();
         String[] s= questionId.split(",");
@@ -89,8 +89,8 @@ public class AppController {
     }
 
 
-    @DeleteMapping("/deleteQuestion/{questionSetId}/{questionId}")
-    public String delete_question_by_id(@PathVariable String questionSetId,@PathVariable String questionId)
+    @DeleteMapping("/deleteQuestion")
+    public String delete_question_by_id(@RequestParam String questionSetId,@RequestParam String questionId)
     {
         StringBuilder sb = new StringBuilder();
         String[] questionIds= questionId.split(",");
@@ -102,8 +102,8 @@ public class AppController {
         return sb.toString();
     }
 
-    @GetMapping("/Evaluate/{candidateId}")
-    public String evaluate(@PathVariable String candidateId)
+    @GetMapping("/Evaluate")
+    public String evaluate(@RequestParam String candidateId)
     {
         return adminRepository.evaluate(candidateId);
     }
@@ -111,28 +111,28 @@ public class AppController {
     @PostMapping("/Assessment/getCandidateId")
     public String get_candidate_id(@RequestBody Candidate candidate)
     {
-        sg3.reset(Long.parseLong(sg3.getSequence().trim()));
-        long id = sg3.nextId();
+        candidateIdGenerator.setId(Long.parseLong(candidateIdGenerator.fetchIdFromDatabase().trim()));
+        long id = candidateIdGenerator.nextId();
         candidate.setCandidate_id(String.valueOf(id));
-        sg3.setSequence(id);
+        candidateIdGenerator.updateIdInDatabase(id);
         return candidateRepository.getCandidateId(candidate);
     }
 
-    @GetMapping("/Assessment/getQuestionSet/{candidateId}")
-    public String get_question_set(@PathVariable String candidateId)
+    @GetMapping("/Assessment/getQuestionSet")
+    public String get_question_set(@RequestParam String candidateId)
     {
         return candidateRepository.getQuestionSet(candidateId);
     }
 
-    @PostMapping("/Assessment/submitAnswerSheet/{candidateId}")
-    public String submit_answer_sheet(@PathVariable String candidateId, @RequestBody Answer answer)
+    @PostMapping("/Assessment/submitAnswerSheet")
+    public String submit_answer_sheet(@RequestParam String candidateId, @RequestBody Answer answer)
     {
-        sg4.reset(Long.parseLong(sg4.getSequence().trim()));
-        long Id = sg4.nextId();
+        answerIdGenerator.setId(Long.parseLong(answerIdGenerator.fetchIdFromDatabase().trim()));
+        long Id = answerIdGenerator.nextId();
         String id = String.valueOf(Id);
         answer.setId(id);
         answer.setCandidate_id(candidateId);
-        sg4.setSequence(Id);
+        answerIdGenerator.updateIdInDatabase(Id);
         return candidateRepository.submitAnswerSheet(candidateId,answer);
     }
 
---
+++ ITO Quiz/PathRangeIndexRepository.java
@@ -0,0 +1,45 @@
+package com.marklogicquery.run.xquery.repository;
+
+import com.marklogicquery.run.xquery.service.MarkLogicConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PathRangeIndexRepository {
+
+    @Autowired
+    MarkLogicConnection mlc;
+
+    public void createPathRangeIndexes() {
+         mlc.executeXQuery("""
+                 import module namespace admin = "http://marklogic.com/xdmp/admin" at "/MarkLogic/admin.xqy";
+                 try{\
+                 let $config := admin:get-configuration()
+                 let $dbid := xdmp:database("Documents")
+                 let $pri := admin:database-range-path-index($dbid,"string","/tXML/Questions/Id","http://marklogic.com/collation/",fn:false(),"ignore")
+                 let $apri := admin:database-add-range-path-index($config, $dbid, $pri)
+                 return admin:save-configuration($apri),\
+                 let $config := admin:get-configuration()
+                 let $dbid := xdmp:database("Documents")
+                 let $pri := admin:database-range-path-index($dbid,"string","/tXML/Questions/Question/questionId","http://marklogic.com/collation/",fn:false(),"ignore")
+                 let $apri := admin:database-add-range-path-index($config, $dbid, $pri)
+                 return admin:save-configuration($apri),\
+                 let $config := admin:get-configuration()
+                 let $dbid := xdmp:database("Documents")
+                 let $pri := admin:database-range-path-index($dbid,"string","/tXML/Candidate/candidate_id","http://marklogic.com/collation/",fn:false(),"ignore")
+                 let $apri := admin:database-add-range-path-index($config, $dbid, $pri)
+                 return admin:save-configuration($apri),\
+                 let $config := admin:get-configuration()
+                 let $dbid := xdmp:database("Documents")
+                 let $pri := admin:database-range-path-index($dbid,"string","/tXML/Answer/Id","http://marklogic.com/collation/",fn:false(),"ignore")
+                 let $apri := admin:database-add-range-path-index($config, $dbid, $pri)
+                 return admin:save-configuration($apri),\
+                 let $config := admin:get-configuration()
+                 let $dbid := xdmp:database("Documents")
+                 let $pri := admin:database-range-path-index($dbid,"string","/tXML/Answer/candidateId","http://marklogic.com/collation/",fn:false(),"ignore")
+                 let $apri := admin:database-add-range-path-index($config, $dbid, $pri)
+                 return admin:save-configuration($apri)}\
+                 catch($e){}""");
+         System.out.println("Path Range Indexes Successfully Created");
+    }
+}
